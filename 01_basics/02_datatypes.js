876ee38c9ff287103c6e1da0d2ee09ccf00666fc
Tara Chand <91619377+c-tarac@users.noreply.github.com>
2025-02-27 08:47:18 +0000

    datatypes added

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code newer version
+
+// alert("hello") // we are using node js not brouser   tc39 and mdn resources
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// // null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+
+console.log(typeof "Tara")
+
+console.log(typeof null)
+
+console.log(typeof undefined)
+
+
