a1adab953382b8178fae162978fc1950da804970
Tara Chand <91619377+c-tarac@users.noreply.github.com>
2025-02-28 06:43:37 +0000

    conversion

---
+++ 01_basics/04_comperisions.js
@@ -0,0 +1,16 @@
+console.log("2">1)
+console.log("02">1)
+
+
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+console.log(undefined == 0)
+console.log(undefined >= 0)
+console.log(undefined > 0)
+
+
+// === check type of datatypes also before comperision
+console.log("2" === 2)
